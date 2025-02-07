63b7864f7c7441355a834bee2be830988bee73b6
uberswe <m@rkus.io>
2025-02-07 10:10:01 +0100

    initial profile work

--- internal/pages/profile.go
+++ internal/pages/profile.go
@@ -1,6 +1,7 @@
 package pages
 
 import (
+	"createmod/internal/models"
 	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"net/http"
@@ -9,18 +10,40 @@ import (
 const profileTemplate = "profile.html"
 
 type ProfileData struct {
+	Username   string
+	Name       string
+	Schematics []models.Schematic
 	DefaultData
 }
 
 func ProfileHandler(app *pocketbase.PocketBase) func(c echo.Context) error {
 	return func(c echo.Context) error {
-		d := ProfileData{}
-		d.Title = ""
-		d.Categories = allCategories(app)
-		err := c.Render(http.StatusOK, profileTemplate, d)
-		if err != nil {
-			return err
+		username := c.PathParam("username")
+		if username == "" {
+			return editProfile(c, app)
 		}
-		return nil
+		return showProfile(c, app, username)
 	}
+}
+
+func showProfile(c echo.Context, app *pocketbase.PocketBase, username string) error {
+	d := ProfileData{}
+	d.Title = ""
+	d.Categories = allCategories(app)
+	err := c.Render(http.StatusOK, profileTemplate, d)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func editProfile(c echo.Context, app *pocketbase.PocketBase) error {
+	d := ProfileData{}
+	d.Title = ""
+	d.Categories = allCategories(app)
+	err := c.Render(http.StatusOK, profileTemplate, d)
+	if err != nil {
+		return err
+	}
+	return nil
 }
