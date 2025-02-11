57506951473c72733c9b26e5b2c497a2840308d4
Uberswe <m@rkus.io>
2025-02-11 23:18:15 +0100

    Remove views batching (#66)

--- internal/migrate/views.go
+++ internal/migrate/views.go
@@ -1,14 +1,12 @@
 package migrate
 
 import (
-	"createmod/model"
 	"createmod/query"
 	"errors"
 	"fmt"
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/models"
-	"gorm.io/gen"
 	"gorm.io/gorm"
 	"log"
 )
@@ -26,57 +24,46 @@ func migrateViews(app *pocketbase.PocketBase, gormdb *gorm.DB, oldUserIDs map[in
 		panic(err)
 	}
 
-	totalCount := res{}
-	countErr := app.Dao().DB().
-		NewQuery("SELECT COUNT(id) as c FROM schematic_views").
-		One(&totalCount)
-	if countErr != nil {
-		panic(countErr)
-	}
+	postViewRes, viewErr := q.QeyKryWEpostView.Find()
+	for i := range postViewRes {
+		if newSchematicID, ok := oldSchematicIDs[postViewRes[i].ID]; ok {
+			filter, err := app.Dao().FindRecordsByFilter(
+				schematicViewsCollection.Id,
+				"old_schematic_id = {:old_schematic_id} && type = {:type}",
+				"-created",
+				1,
+				0,
+				dbx.Params{
+					"old_schematic_id": postViewRes[i].ID,
+					"type":             postViewRes[i].Type,
+				})
+			if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+				app.Logger().Debug(
+					fmt.Sprintf("Views error: %v", err),
+					"filter-len", len(filter),
+				)
+				continue
+			} else if err == nil && len(filter) != 0 {
+				app.Logger().Debug(
+					fmt.Sprintf("Views found: %v", err),
+					"filter-len", len(filter),
+				)
+				continue
+			}
 
-	postViewRes := make([]*model.QeyKryWEpostView, 0)
-	postErr := q.QeyKryWEpostView.FindInBatches(&postViewRes, 25000, func(tx gen.Dao, batch int) error {
-		for i := range postViewRes {
-			if newSchematicID, ok := oldSchematicIDs[postViewRes[i].ID]; ok {
-				filter, err := app.Dao().FindRecordsByFilter(
-					schematicViewsCollection.Id,
-					"old_schematic_id = {:old_schematic_id} && type = {:type}",
-					"-created",
-					1,
-					0,
-					dbx.Params{
-						"old_schematic_id": postViewRes[i].ID,
-						"type":             postViewRes[i].Type,
-					})
-				if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
-					app.Logger().Debug(
-						fmt.Sprintf("Views error: %v", err),
-						"filter-len", len(filter),
-					)
-					continue
-				} else if err == nil && len(filter) != 0 {
-					app.Logger().Debug(
-						fmt.Sprintf("Views found: %v", err),
-						"filter-len", len(filter),
-					)
-					return nil
-				}
+			record := models.NewRecord(schematicViewsCollection)
+			record.Set("old_schematic_id", postViewRes[i].ID)
+			record.Set("schematic", newSchematicID)
+			record.Set("count", postViewRes[i].Count_)
+			record.Set("type", postViewRes[i].Type)
+			record.Set("period", postViewRes[i].Period)
 
-				record := models.NewRecord(schematicViewsCollection)
-				record.Set("old_schematic_id", postViewRes[i].ID)
-				record.Set("schematic", newSchematicID)
-				record.Set("count", postViewRes[i].Count_)
-				record.Set("type", postViewRes[i].Type)
-				record.Set("period", postViewRes[i].Period)
-
-				if err = app.Dao().SaveRecord(record); err != nil {
-					panic(err)
-				}
+			if err = app.Dao().SaveRecord(record); err != nil {
+				panic(err)
 			}
 		}
-		return nil
-	})
-	if postErr != nil {
-		panic(postErr)
+	}
+	if viewErr != nil {
+		panic(viewErr)
 	}
 }
