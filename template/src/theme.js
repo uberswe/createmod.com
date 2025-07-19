49c9cba4e707b03568c5d052d2061bef1435063c
uberswe <admin@uberswe.com>
2025-07-20 00:24:54 +0200

    fix: dark mode toggle and dropdown bug have been fixed

--- template/src/theme.js
+++ template/src/theme.js
@@ -16,12 +16,24 @@
         selectedTheme = params.theme;
     } else {
         var storedTheme = localStorage.getItem(themeStorageKey);
+        if (storedTheme == null) {
+            localStorage.setItem(themeStorageKey, defaultTheme);
+        }
         selectedTheme = storedTheme ? storedTheme : defaultTheme;
     }
+    console.log("selected theme" + selectedTheme)
     if (selectedTheme === 'dark') {
         document.body.setAttribute("data-bs-theme", selectedTheme);
+        // Remove hide-theme-light classes when theme is dark
+        document.querySelectorAll('.hide-theme-light').forEach(function(element) {
+            element.classList.remove('hide-theme-light');
+        });
     } else {
-        document.body.removeAttribute("data-bs-theme");
+        document.body.setAttribute("data-bs-theme", selectedTheme);
+        // Remove hide-theme-dark classes when theme is not dark
+        document.querySelectorAll('.hide-theme-dark').forEach(function(element) {
+            element.classList.remove('hide-theme-dark');
+        });
     }
 
 }));
