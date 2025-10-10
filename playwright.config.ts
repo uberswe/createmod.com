aaf190ef24ad853b5b7aaf7f8417b45a1ccbea4c
uberswe <admin@uberswe.com>
2025-10-10 15:52:06 +0200

    chore: add testing files

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests/e2e',
+  timeout: 60_000,
+  retries: 1,
+  use: {
+    baseURL: process.env.APP_BASE_URL || 'http://localhost:8080',
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+  ],
+  workers: 3,
+});
